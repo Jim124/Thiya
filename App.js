9d946186817dc3eda4fc4d63cf6402136447a057
Jim124 <fengpingan065@gmail.com>
2024-05-30 10:39:41 +0700

    add style to CategoryItem

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import CategoryScreen from './screen/CategoryScreen';
 export default function App() {
-  return <CategoryScreen />;
+  return (
+    <>
+      <StatusBar style='light' />
+      <CategoryScreen />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
--- components/CategoryItem.js
+++ components/CategoryItem.js
@@ -1,13 +1,52 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Platform } from 'react-native';
 function CategoryItem({ title, color }) {
   return (
-    <View>
-      <Pressable>
-        <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        android_ripple={{ color: '#ccc' }}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
       </Pressable>
     </View>
   );
 }
 
 export default CategoryItem;
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { with: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visable',
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
