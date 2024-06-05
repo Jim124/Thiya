7117cadd3497d8d22aeea8865ba4fc46b508b448
Jim124 <fengpingan065@gmail.com>
2024-06-05 15:26:39 +0800

    add headerRight button

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
+import IconButton from './components/ui/IconButton';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
@@ -11,7 +12,20 @@ export default function App() {
       <StatusBar style='auto' />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='AllPlaces' component={AllPlaces} />
+          <Stack.Screen
+            name='AllPlaces'
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon='add'
+                  color={tintColor}
+                  size={24}
+                  onPress={() => navigation.navigate('Add a Place')}
+                />
+              ),
+            })}
+          />
           <Stack.Screen name='Add a Place' component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>
