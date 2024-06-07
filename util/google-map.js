fd72464b46f6a2402581e8c07ec5ef662cb4c4e8
Jim124 <fengpingan065@gmail.com>
2024-06-07 10:48:21 +0800

    modify getAddress function

--- util/google-map.js
+++ util/google-map.js
@@ -9,6 +9,7 @@ export async function getAddress(lat, lng) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_API_KEY}`;
   try {
     const response = await fetch(url);
+    if (!response.ok) throw new Error('Failed to fetch address!');
     const data = await response.json();
     return data.results[0].formatted_address;
   } catch (error) {
