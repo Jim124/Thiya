36f072da42878f1eb00c6bab2fe4be87d2f83375
Jim124 <fengpingan065@gmail.com>
2024-06-09 13:10:16 +0800

    initialize NOTIFICATION project

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 export default function App() {
+  Notifications.getPermissionsAsync();
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'my first local notificaton',
+        body: 'this is the body of the notification',
+        data: { username: 'Jim' },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello,World</Text>
-      <StatusBar style="auto" />
+      <Button
+        title='Shedule Notification'
+        onPress={scheduleNotificationHandler}
+      />
+      <StatusBar style='auto' />
     </View>
   );
 }
